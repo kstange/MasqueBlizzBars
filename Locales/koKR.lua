4f5d12eb271060dc43c3c35570ac321fca67d21b
Kevin Stange <kstange@gmail.com>
2026-06-07 15:51:59 -0500

    Fix a missed change in a comment

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -1,7 +1,7 @@
 --
 -- Masque Blizzard Bars
 --
--- Locales\koKR.lua -- ruRU Localization File
+-- Locales\koKR.lua -- koKR Localization File
 --
 -- Use of this source code is governed by an MIT-style
 -- license that can be found in the LICENSE file or at
